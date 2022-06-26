2c5fa9209094986ca4374e184fcf0f5ccf884327
Nikolay <admin@MacBook-Air-Alexey.local>
2022-06-26 18:35:42 +0300

    update

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ let div = document.querySelector('.output');
 
 buttonIn.onclick = function(){
         let valueInput = inputIn.value;
-        div.innerHTML = 'Привет, ' + valueInput;
+        div.innerHTML = 'Привет, ' + valueInput + '!';
     
 };
 document.addEventListener('keydown', function(event) {
     if (event.code == 'Enter') {
         let valueInput = inputIn.value;
-        div.innerHTML = 'Привет, ' + valueInput;
+        div.innerHTML = 'Привет, ' + valueInput + '!';
     }
 });
