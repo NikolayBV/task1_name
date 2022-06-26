722577aaa6a62ab7eb4576da1aa2eda7024961e8
Nikolay <admin@MacBook-Air-Alexey.local>
2022-06-26 18:32:15 +0300

    update

--- script.js
+++ script.js
@@ -3,14 +3,9 @@ let buttonIn = document.querySelector('.button-in');
 let div = document.querySelector('.output');
 
 buttonIn.onclick = function(){
-    if(KeyboardEvent ===13){
         let valueInput = inputIn.value;
         div.innerHTML = 'Привет, ' + valueInput;
-    }
-    else{
-        let valueInput = inputIn.value;
-        div.innerHTML = 'Привет, ' + valueInput;
-    }
+    
 };
 document.addEventListener('keydown', function(event) {
     if (event.code == 'Enter') {
