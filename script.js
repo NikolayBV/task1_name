a2bf61645dbde9d1fac5b38cc918cd9a8e90e4ca
NikolayBV <bnv95o@yandex.ru>
2022-06-30 17:47:33 +0300

    added window.onload

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let inputIn = document.querySelector('.input-in');
-let buttonIn = document.querySelector('.button-in');
-let div = document.querySelector('.output');
+window.onload = function() {
+    let inputIn = document.querySelector('.input-in');
+    let buttonIn = document.querySelector('.button-in');
+    let div = document.querySelector('.output');
 
-buttonIn.onclick = function(){
-        let valueInput = inputIn.value;
-        div.innerHTML = 'Привет, ' + valueInput + '!';
-    
+    buttonIn.onclick = function(){
+            let valueInput = inputIn.value;
+            div.innerHTML = 'Привет, ' + valueInput + '!';
+        
+    };
+    document.addEventListener('keydown', function(event) {
+        if (event.code == 'Enter') {
+            let valueInput = inputIn.value;
+            div.innerHTML = 'Привет, ' + valueInput + '!';
+        }
+    })
 };
-document.addEventListener('keydown', function(event) {
-    if (event.code == 'Enter') {
-        let valueInput = inputIn.value;
-        div.innerHTML = 'Привет, ' + valueInput + '!';
-    }
-});
